b20b7d67d48230adc03411e09d462f63297d3d0f
Ariana <arianarsdev@gmail.com>
2015-03-04 12:14:11 -0600

    Material Design Sliding Tabs

--- app/src/main/java/com/example/ariana/slidingtabs/WelcomeActivity.java
+++ app/src/main/java/com/example/ariana/slidingtabs/WelcomeActivity.java
@@ -1,13 +1,23 @@
 package com.example.ariana.slidingtabs;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.ariana.slidingtabs.adapter.ViewPagerAdapter;
+import com.example.ariana.slidingtabs.iosched.SlidingTabLayout;
+
 
 public class WelcomeActivity extends ActionBarActivity {
+
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
+    SlidingTabLayout slidingTabLayout;
+    CharSequence Titles[]={"Books","Ebooks"};
+    int Numboftabs = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +27,23 @@ public class WelcomeActivity extends ActionBarActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_awesome_toolbar);
         toolbar.setTitle("Sliding Tabs - Google I/O");
         setSupportActionBar(toolbar);
-    }
 
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles, Numboftabs);
+        viewPager = (ViewPager) findViewById(R.id.my_awesome_pager);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.my_awesome_tabs);
+        slidingTabLayout.setDistributeEvenly(true);
+        slidingTabLayout.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
+            @Override
+            public int getIndicatorColor(int position) {
+                return getResources().getColor(R.color.tabsScrollColor);
+            }
+        });
+
+        // Setting the ViewPager For the SlidingTabsLayout
+        slidingTabLayout.setViewPager(viewPager);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/ariana/slidingtabs/adapter/ViewPagerAdapter.java
@@ -0,0 +1,58 @@
+package com.example.ariana.slidingtabs.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.example.ariana.slidingtabs.fragments.BooksFragment;
+import com.example.ariana.slidingtabs.fragments.EbooksFragment;
+
+/**
+ * Created by ariana on 4/03/15.
+ */
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    CharSequence Titles[]; // This will Store the Titles of the Tabs which are Going to be passed when ViewPagerAdapter is created
+    int NumbOfTabs; // Store the number of tabs, this will also be passed when the ViewPagerAdapter is created
+
+
+    // Build a Constructor and assign the passed Values to appropriate values in the class
+    public ViewPagerAdapter(FragmentManager fragmentManager ,CharSequence mTitles[], int mNumbOfTabsumb) {
+        super(fragmentManager);
+
+        this.Titles = mTitles;
+        this.NumbOfTabs = mNumbOfTabsumb;
+
+    }
+
+    //This method return the fragment for the every position in the View Pager
+    @Override
+    public Fragment getItem(int position) {
+
+        if(position == 0) // if the position is 0 we are returning the First tab
+        {
+            BooksFragment booksFragment = new BooksFragment();
+            return booksFragment;
+        }
+        else             // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
+        {
+            EbooksFragment ebooksFragment = new EbooksFragment();
+            return ebooksFragment;
+        }
+
+    }
+
+    // This method return the titles for the Tabs in the Tab Strip
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return Titles[position];
+    }
+
+    // This method return the Number of tabs for the tabs Strip
+
+    @Override
+    public int getCount() {
+        return NumbOfTabs;
+    }
+}
