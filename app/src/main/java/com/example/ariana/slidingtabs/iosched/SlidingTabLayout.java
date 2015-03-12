970a6a6abfc513763c2fde8dd1ccfa552f888296
Ariana <arianarsdev@gmail.com>
2015-03-12 17:28:03 -0600

    Change the tab title color

--- app/src/main/java/com/example/ariana/slidingtabs/iosched/SlidingTabLayout.java
+++ app/src/main/java/com/example/ariana/slidingtabs/iosched/SlidingTabLayout.java
@@ -16,6 +16,7 @@ package com.example.ariana.slidingtabs.iosched;
  * limitations under the License.
  */
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -211,6 +212,7 @@ public class SlidingTabLayout extends HorizontalScrollView {
             }
 
             tabTitleView.setText(adapter.getPageTitle(i));
+            tabTitleView.setTextColor(Color.WHITE);
             tabView.setOnClickListener(tabClickListener);
             String desc = mContentDescriptions.get(i, null);
             if (desc != null) {
